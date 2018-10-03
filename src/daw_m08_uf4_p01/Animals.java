25ef13b6e8ca385b9d71f8f85a33fbc70b208f61
LAPTOP-NTCGK8E9\alexa <alexalba123@gmail.com>
2018-10-03 16:30:00 +0200

    Afegir mètode canviNom

---
+++ src/daw_m08_uf4_p01/Animals.java
@@ -0,0 +1,14 @@
+package daw_m08_uf4_p01;
+
+public class Animals {
+    
+    String nom;
+
+    public Animals(String nom){
+        this.nom = nom;
+    }
+    
+    public void canviNom(){
+    
+    }
+}
